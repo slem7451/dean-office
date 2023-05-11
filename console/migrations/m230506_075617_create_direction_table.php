8b3d51f5ef133e37a6845532ae2f7a5aea4dbaa2
slem7451 <slem7451@gmail.com>
2023-05-11 22:33:57 +0500

    fix DB

--- console/migrations/m230506_075617_create_direction_table.php
+++ console/migrations/m230506_075617_create_direction_table.php
@@ -14,7 +14,11 @@ class m230506_075617_create_direction_table extends Migration
     {
         $this->createTable('{{%direction}}', [
             'id' => $this->string()->unique(),
-            'name' => $this->string()->notNull()
+            'full_name' => $this->string()->notNull(),
+            'short_name' => $this->string()->notNull(),
+            'academic_name' => $this->string()->notNull(),
+            'profile' => $this->string()->notNull()
+
         ]);
 
         $this->addPrimaryKey('direction-pk', 'direction', ['id']);
